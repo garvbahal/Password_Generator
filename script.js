cc7714edb0cdad145e603a141935cf3ea2aa9944
garvbahal <garvbahal75@gmail.com>
2025-01-09 02:13:52 +0530

    first-commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const lengthDisplay = document.querySelector("[data-lengthNumber]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");
 
 const copyBtn = document.querySelector("[data-copy]");
-const copyMsg = document.querySelector("[data-copyMsg");
+const copyMsg = document.querySelector("[data-copyMsg]");
 const uppercaseCheck = document.querySelector("#uppercase");
 const lowercaseCheck = document.querySelector("#lowercase");
 const numbersCheck = document.querySelector("#numbers");
@@ -52,7 +52,50 @@ function generateSymbol() {
   return symbols.charAt(randomNum);
 }
 
-function calcStrength(){
-  
+function calcStrength() {
+  let hasUpper = false;
+  let hasLower = false;
+  let hasNum = false;
+  let hasSym = false;
 
+  if (uppercaseCheck.checked) {
+    hasUpper = true;
+  }
+  if (lowercaseCheck.checked) {
+    hasLower = true;
+  }
+  if (symbolsCheck.checked) {
+    hasSym = true;
+  }
+  if (numbersCheck.checked) {
+    hasNum = true;
+  }
+
+  if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+    setIndicator("#0f0");
+  } else if (
+    (hasLower || hasUpper) &&
+    (hasNum || hasSym) &&
+    passwordLength >= 6
+  ) {
+    setIndicator("#ff0");
+  } else {
+    setIndicator("#f00");
+  }
+}
+
+async function copyContent() {
+  try {
+    await navigator.clipboard.writeText(passwordDisplay.value);
+    copyMsg.innerText = "copied";
+  } catch (e) {
+    copyMsg.innerText = "Failed";
+  }
+  // to make copy span visible
+  copyMsg.classList.add("active");
+
+  setTimeout(() => {
+    copyMsg.classList.remove("active");
+  }, 2000);
+  
 }
