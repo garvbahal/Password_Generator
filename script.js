b04183e124f74de3d4e8627d8d156b416804da15
garvbahal <garvbahal75@gmail.com>
2025-01-09 01:43:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+
+const copyBtn = document.querySelector("[data-copy]");
+const copyMsg = document.querySelector("[data-copyMsg");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#numbers");
+const symbolsCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+
+const symbols = "~`!@#$%^&*()_-+={[]}|:<,.>?/;'";
+
+// starting values
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
+
+handleSlider();
+
+function handleSlider() {
+  inputSlider.value = passwordLength;
+  lengthDisplay.innerText = passwordLength;
+}
+
+function setIndicator(color) {
+  indicator.style.backgroundColor = color;
+  // shadow H/W
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateRandomNumber() {
+  return getRandomInteger(0, 9);
+}
+
+function generateLowerCase() {
+  return String.fromCharCode(getRandomInteger(97, 123));
+}
+
+function generateUpperCase() {
+  return String.fromCharCode(getRandomInteger(65, 91));
+}
+
+function generateSymbol() {
+  const randomNum = getRandomInteger(0, symbols.length);
+  return symbols.charAt(randomNum);
+}
+
+function calcStrength(){
+  
+
+}
